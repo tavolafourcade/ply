cf77c430638ab6e2e55b981b02ca55ce7a4ed348
tavolafourcade <tavolafourcade@gmail.com>
2022-04-14 12:33:55 -0500

    [ ADD ] shortUrl Endpoint [ ADD ] local connection to Mongo DB

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
+
+mongoose.connect('mongodb://localhost/urlShortener', {
+    // Avoid deprecation warnings:
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
 app.set('view engine', 'ejs') // Using EJS as the view engine
 
@@ -8,3 +15,9 @@ app.get('/', (req,res)=> {
 })
 
 app.listen(process.env.PORT || 3000) // Default port
+
+// Creating short Url Endpoint: /shortUrls
+
+app.post('/shortUrls', (req,res) => {
+    // Connect to DB and save a new short Url
+})
